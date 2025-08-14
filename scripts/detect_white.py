c94e0c479a30afb3b5f38c5664612bacc393c667
郑宇杰 <22241014>
2025-08-14 13:53:35 +0800

    update shejibaogao

--- scripts/detect_white.py
+++ scripts/detect_white.py
@@ -386,7 +386,7 @@ class ObjectTrackingHoverNode:
                     center = self.motion_model['center']
                     radius = self.motion_model['radius']
                     
-                    self.intercept_point = np.array([center[0] + radius, center[1]])
+                    self.intercept_point = np.array([center[0], center[1] - radius])
                     #drone_pos_xy = np.array([self.drone_pose.pose.position.x, self.drone_pose.pose.position.y])
                     
                     #vec_to_drone = drone_pos_xy - center
@@ -439,7 +439,7 @@ class ObjectTrackingHoverNode:
                 if self.target is not None:
                     pose_msg = Pose()
                     pose_msg.position.x = self.intercept_point[0] - 1.23
-                    pose_msg.position.y = self.intercept_point[1] + 3
+                    pose_msg.position.y = self.intercept_point[1] + 1
                     pose_msg.position.z = 0.0
                     self.pose_publishers['white'].publish(pose_msg)
                     rospy.loginfo(f"SUCCESS: Target ['white'] pose ({self.intercept_point[0], self.intercept_point[1]})published.")
